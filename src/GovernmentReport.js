1df2b0b2a22f0d887019b7262a9d455d177ba0ab
morganwest <mrw6863@g.rit.edu>
2021-04-26 09:51:18 -0400

    updated government report page css

--- src/GovernmentReport.js
+++ src/GovernmentReport.js
@@ -1,59 +1,269 @@
 import './css/Home.css';
 import Button from "./Button"
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
 import {Input, InputLabel} from "@material-ui/core";
+import {Component} from "react";
+import styled from "styled-components";
+import Styles from "./Styles";
+import React from 'react';
+import Colors from "./Colors";
+import {CenteredColumn} from "./StyleBlocks";
 
-const GovernmentReport = () => {
+const StyledReport = styled(CenteredColumn)`
+  padding: 0 64px;
+  ${Styles.centeredColumn};
+  align-content: center;
+  
+  Div{
+    padding: 10px;
+  }
+  
+  #submit{
+    outline: 3px solid black;
+  }
+  
+  #title{
+    h1{
+      ${Styles.header}
+      font-style: italic;
+    }
+  }
+  
+  #form{
+    ${Styles.darkOutline};
+    background-color: ${Colors.green2};
+  }
+  
+  #btn{
+    background-color: ${Colors.green2};
+    padding: 6px;
+    margin: 6px;
+    ${Styles.darkOutline};
+  }
+  
+  #btns{
+    margin: 6px;
+  }
+  
+  #other{
+    label{
+      margin: 6px;
+      padding: 8px;
+    }
+  }
+  
+  #formText{
+    padding: 12px;
+    font-weight: bold;
+    margin: 12px;
+  }
+  
+  #locationForm{
+    margin-top: 8px;
+    margin-bottom: 20px;
+    button{
+      margin: 12px;
+      ${Styles.darkOutline};
+      background: ${Colors.green2};
+    }
+  }
+  
+  #facility {
+    //outline: 3px solid pink;
+  }
+  
+`;
 
-    return (
-        <div className="GovernmentReport">
-            <header className="government-report-header">
-                <h1>Manage Reports</h1>
-            </header>
-            <body>
-            <form>
-                <Button label="Use My Address"/><br/><br/>
-                <Button label="Use Other Address"/><br/><br/>
+class GovernmentReport extends Component {
 
-                <form>
-                    <InputLabel className="addressLine1-input-label">Address:</InputLabel>
-                    <Input id="adr1" disabled={true} type="text" className="addressLine1-input"></Input><br/><br/>
+    constructor(props) {
+        super(props);
+        console.log(this.props)
+        this.state = {
+            email: "",
+            password: "",
+            address: "",
+            address_l2: "",
+            zipCode: "",
+            city: "",
+            state: "",
+            facility: "",
+            details: "",
+            government_id:""
+        }
+    }
 
-                    <InputLabel className="addressLine2-input-label">Address L2 (optional):</InputLabel>
-                    <Input id="adr2" disabled={true} type="text" className="addressLine2-input"></Input><br/><br/>
+    render() {
+        return (
+            <StyledReport>
+                <div className="GovernmentReport">
+                    <header className="government-report-header" id={"title"}>
+                        <h1>Report Issue</h1>
+                    </header>
+                    <body id = "form">
 
-                    <InputLabel className="zipcode-input-label">Zipcode:</InputLabel>
-                    <Input id="zipcode" disabled={true} type="text" className="zipcode-input"></Input><br/><br/>
+                    <div id={"btns"}>
+                        <button onClick={() => this.setOtherLocationDisable()} id={"btn"}>Use My Address</button><br/>
+                        <button className="other-location-button" onClick={() => this.setOtherLocationEnable()} id={"btn"}>Use Other Address</button><br/>
+                    </div>
 
-                    <InputLabel className="city-input-label">City:</InputLabel>
-                    <Input id="city" disabled={true} type="text" className="city-input"></Input><br/><br/>
+                    <div id={"other"}>
+                        <label>Other Location:</label><br/>
 
-                    <InputLabel className="state-input-label">State:</InputLabel>
-                    <Input id="state" disabled={true} type="text" className="state-input"></Input><br/><br/>
-                </form>
+                        <div id={"locationForm"}>
+                            {this.state.addressEmpty && <label className="error-msg">Address field is empty</label>}
+                            <InputLabel className="addressLine1-input-label">Address:</InputLabel>
+                            <input id="adr1" disabled={true} type="text" className="addressLine1-input" onChange={evt => this.newAddressLine1(evt)}/><br/><br/>
 
-                <InputLabel className={"facility-label"}>Select Facility:</InputLabel>
-                <Dropdown
-                    placeholder="-/-"
-                    className="facility-dropdown"
-                    options={['Electric', 'Roads', 'Water']}
-                    value="Electric"
-                    onChange={(value) => console.log('change!', value)}
-                    onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
-                    onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
-                    onOpen={() => console.log('open!')}
-                />
+                            <InputLabel className="addressLine2-input-label">Address L2 (optional):</InputLabel>
+                            <input id="adr2" disabled={true} type="text" className="addressLine2-input" onChange={evt => this.newAddressLine2(evt)}/><br/><br/>
 
-                <InputLabel className="state-input-label">Details:</InputLabel>
-                <Input type="text" className="details-input"></Input><br/><br/>
+                            {this.state.zipCodeEmpty && <label className="error-msg">Zipcode field is empty</label>}
+                            <InputLabel className="zipcode-input-label">Zipcode:</InputLabel>
+                            <input id="zipcode" disabled={true} type="text" className="zipcode-input" onChange={evt => this.newZipcode(evt)}/><br/><br/>
 
-                <Button label="Submit" to="/citizen-home"/>
+                            {this.state.cityEmpty && <label className="error-msg">City field is empty</label>}
+                            <InputLabel className="city-input-label">City:</InputLabel>
+                            <input id="city" disabled={true} type="text" className="city-input" onChange={evt => this.newCity(evt)}/><br/><br/>
 
-            </form>
-            </body>
-        </div>
-    );
+                            {this.state.stateEmpty && <label className="error-msg">State field is empty</label>}
+                            <InputLabel className="state-input-label" >State:</InputLabel>
+                            <input id="state" disabled={true} type="text" className="state-input" onChange={evt => this.newState(evt)}/><br/><br/>
+
+                            {this.state.facilityEmpty && <label className="error-msg">Facility not chosen</label>}<br/>
+                            <label>Select Facility:</label><br/>
+                            <button id={"btn"} onClick={() => this.setElectric()}> Electric </button>
+                            <button id={"btn"} onClick={() => this.setRoads()}> Roads </button>
+                            <button id={"btn"} onClick={() => this.setWater()}> Water </button><br/>
+                            <label id="facility">Chosen Facility: N/A</label><br/><br/>
+
+                        </div>
+
+                        {this.state.detailsEmpty && <label className="error-msg">Details field is empty</label>}
+                        <InputLabel id={"formText"} className="state-input-label">Details:</InputLabel>
+                        <Input type="text" className="details-input" onChange={evt => this.updateDetails(evt)}/><br/><br/>
+
+                        <button id={"btn"} className="validate-button" onClick={() => this.submit()}>Verify</button><br/><br/>
+
+                    </div>
+
+                    <Button id={"submit"} label="Submit" to="/government-home"/>
+                    </body>
+                </div>
+            </StyledReport>
+        );
+    }
+
+    setOtherLocationDisable(){
+        if(document.getElementById("adr1").disabled === false){
+            document.getElementById("adr1").disabled = true;
+        }
+
+        if(document.getElementById("adr2").disabled === false){
+            document.getElementById("adr2").disabled = true
+        }
+
+        if(document.getElementById("zipcode").disabled === false){
+            document.getElementById("zipcode").disabled = true
+        }
+
+        if(document.getElementById("city").disabled === false){
+            document.getElementById("city").disabled = true;
+        }
+
+        if(document.getElementById("state").disabled === false){
+            document.getElementById("state").disabled = true;
+        }
+
+    }
+
+    setOtherLocationEnable(){
+
+        if(document.getElementById("adr1").disabled === true){
+            document.getElementById("adr1").disabled = false;
+        }
+
+        if(document.getElementById("adr2").disabled === true){
+            document.getElementById("adr2").disabled = false;
+        }
+
+        if(document.getElementById("zipcode").disabled === true){
+            document.getElementById("zipcode").disabled = false;
+        }
+
+        if(document.getElementById("city").disabled === true){
+            document.getElementById("city").disabled = false;
+        }
+
+        if(document.getElementById("state").disabled === true){
+            document.getElementById("state").disabled = false;
+        }
+    }
+
+    newAddressLine1(evt) {
+        this.setState({address: evt.target.value});
+    }
+
+    newAddressLine2(evt) {
+        this.setState({address_l2: evt.target.value});
+    }
+
+    newZipcode(evt) {
+        this.setState({zipCode: evt.target.value});
+    }
+
+    newCity(evt) {
+        this.setState({city: evt.target.value});
+    }
+
+    newState(evt) {
+        this.setState({state: evt.target.value});
+    }
+
+    setElectric() {
+        this.setState({facility: "Electric"})
+        document.getElementById("facility").innerText = "Chosen Facility: Electric"
+    }
+
+    setRoads() {
+        this.setState({facility: "Roads"})
+        document.getElementById("facility").innerText = "Chosen Facility: Roads"
+    }
+
+    setWater() {
+        this.setState({facility: "Water"})
+        document.getElementById("facility").innerText = "Chosen Facility: Water"
+    }
+    updateDetails(evt) {
+        this.setState({details: evt.target.value});
+    }
+
+    submit(){
+
+        if(this.state.address.length === 0 && document.getElementById("adr1").disabled === false){
+            this.setState({addressLine1Empty: true});
+        } else this.setState({addressLine1Empty: false});
+
+        if(this.state.zipCode.length === 0 && document.getElementById("adr2").disabled === false){
+            this.setState({zipcodeEmpty: true});
+        } else this.setState({zipcodeEmpty: false});
+
+        if(this.state.city.length === 0 && document.getElementById("city").disabled === false){
+            this.setState({cityEmpty: true});
+        } else this.setState({cityEmpty: false});
+
+        if(this.state.state.length === 0 && document.getElementById("state").disabled === false){
+            this.setState({stateEmpty: true});
+        } else this.setState({stateEmpty: false});
+
+        if(this.state.details.length === 0){
+            this.setState({detailsEmpty: true});
+        } else this.setState({detailsEmpty: false});
+
+        if(this.state.facility.length === 0){
+            this.setState({facilityEmpty: true});
+        } else this.setState({facilityEmpty: false});
+    }
+
 }
 
 export default GovernmentReport;

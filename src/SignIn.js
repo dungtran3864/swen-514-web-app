466cf4b0e5bf2f1026e943a4f3fe73fbf4acf7c9
morganwest <mrw6863@g.rit.edu>
2021-04-26 00:01:26 -0400

    updated sign in page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,28 +1,90 @@
 import './css/Home.css';
 import {Input, InputLabel} from "@material-ui/core";
 import Button from "./Button"
+import Styles from "./Styles";
+import { CenteredColumn } from "./StyleBlocks";
+import styled from "styled-components";
+import React from 'react';
+import Colors from "./Colors";
+
+const StyledSignIn = styled(CenteredColumn)`
+  padding: 0 64px;
+  background-color: ${Colors.green1};
+  ${Styles.centeredColumn};
+  
+  #title{
+    h1{
+      ${Styles.header}
+      font-style: italic;
+    }
+  }
+  
+  #login {
+    text-align: center;
+    padding: 6px;
+    background-color: ${Colors.green2};
+    ${Styles.darkOutline};
+  }
+  
+  #email{
+    padding-top: 12px;
+  }
+  
+  #emailIn{
+    padding: 6px;
+  }
+  
+  #password {
+    padding-top: 12px;
+  }
+  
+  #passwordIn{
+    padding: 6px;
+  }
+
+  #buttons{
+    align: center;
+    display: flex;
+    padding: 12px;
+    ${Styles.centeredRow};
+    
+    #signOut{
+      background-color: ${Colors.red};
+    }
+    
+    #or{
+      padding: 8px;
+      align: center;
+    }
+  }
+` ;
 
 const SignIn = () =>  {
     return (
-        <div className="SignIn">
-            <header className="sign-in-header">
+        <StyledSignIn>
+            <header className="sign-in-header" id={"title"}>
                 <h1>Sign In</h1>
             </header>
             <body>
                 <form>
-                    <InputLabel htmlFor="email">Email:</InputLabel><br/>
-                    <Input type="text" id="email" name="email"/><br/>
-                    <InputLabel htmlFor="password">Password:</InputLabel><br/>
-                    <Input type="password" id="password" name="password"/><br/><br/>
-                    <Button to={"/citizen-home"} label={"Submit"}/><br/><br/>
-                    <label> -OR- </label><br/><br/>
-                    <Button to={"/create-account"} label={"Create Account"}/><br/><br/>
+                    <div id={"login"}>
+                        <InputLabel htmlFor="email" id={"email"}>Email:</InputLabel><br/>
+                        <Input type="text" id="emailIn" name="email"/><br/>
+                        <InputLabel htmlFor="password" id={"password"}>Password:</InputLabel><br/>
+                        <Input type="password" id="passwordIn" name="password"/><br/><br/>
+                    </div>
+
+                    <div id={"buttons"}>
+                        <Button to={"/citizen-home"} label={"Submit"}/><br/><br/>
+                        <label id={"or"}> -OR- </label><br/><br/>
+                        <Button to={"/create-account"} label={"Create Account"}/><br/><br/>
+                    </div>
 
                     <label>================================</label><br/><br/>
                     <Button to={"/"} label={"Back"}/>
                 </form>
             </body>
-        </div>
+        </StyledSignIn>
     );
 }
 

f6839e6b93705b83679529a9e48ba440738299d6
Dung <dxt6193@rit.edu>
2021-04-30 22:49:49 -0400

    Refactor API URl

--- src/API.js
+++ src/API.js
@@ -1,21 +1,21 @@
 // ALL OF THE API URLs USED IN THIS PROJECT:
 
 // Base API URL
-export const baseURL = "https://itpuavz5l8.execute-api.us-east-1.amazonaws.com";
+export const baseURL = "https://h8v29e606g.execute-api.us-east-1.amazonaws.com/dev";
 
 // GET citizen
-export const getCitizen = baseURL + "/dev/citizen/user?citizen_id=";
+export const getCitizen = baseURL + "/citizen/user?citizen_id=";
 
 // POST citizen
-export const postCitizen = baseURL + "/dev/citizen/report";
+export const postCitizen = baseURL + "/citizen/report";
 
 // POST sign up
-export const postSignUp = baseURL + "/dev/citizen/sign-up";
+export const postSignUp = baseURL + "/citizen/sign-up";
 
 // POST log in
-export const postLogIn = baseURL + "/dev/login";
+export const postLogIn = baseURL + "/login";
 
 // GET reports given a citizen's id
-export const getCitizenReports = baseURL + "/dev/citizen/reports?citizen_id=";
+export const getCitizenReports = baseURL + "/citizen/reports?citizen_id=";
 
 export default getCitizen;

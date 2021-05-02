3c942f1d854499e625c2f2ac9f0751401259eeef
Dung <dxt6193@rit.edu>
2021-05-02 17:29:09 -0400

    Fix API

--- src/API.js
+++ src/API.js
@@ -1,21 +1,23 @@
 // ALL OF THE API URLs USED IN THIS PROJECT:
 
 // BASE URL
-export const baseURL = "https://itpuavz5l8.execute-api.us-east-1.amazonaws.com/";
+export const baseURL = "https://itpuavz5l8.execute-api.us-east-1.amazonaws.com/dev";
 
 // GET citizen
-export const getCitizen = baseURL + "dev/citizen/user?citizen_id=";
+export const getCitizen = baseURL + "/citizen/user?citizen_id=";
 
 // POST citizen
-export const postCitizen = baseURL + "dev/citizen/report";
+export const postCitizen = baseURL + "/citizen/report";
 
 // POST sign up
-export const postSignUp = baseURL + "dev/citizen/sign-up";
+export const postSignUp = baseURL + "/citizen/sign-up";
 
 // POST log in
-export const postLogIn = baseURL + "dev/login";
+export const postLogIn = baseURL + "/login";
 
 // GET reports given a citizen's id
-export const getCitizenReports = baseURL + "dev/citizen/reports?citizen_id=";
+export const getCitizenReports = baseURL + "/citizen/reports?citizen_id=";
+
+export const updateReport = baseURL + "/government/report";
 
 export default baseURL;
